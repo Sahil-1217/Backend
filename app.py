d9ea0e162a77d335b0d989f6d2dd4483c7d95d72
Sahil <sahilbhoye15@gmail.com>
2025-01-27 20:57:33 +0530

    Second-commit

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import numpy as np
 import pickle
-from flask import url_for
-from flask import Flask, render_template
 
-
-app = Flask(__name__)
+# Initialize the Flask app
 app = Flask(__name__, static_folder='static')
 
-
+# Load models and scalers
 diabetesModel = pickle.load(open('DibetiesModel.pkl','rb'))
-
 heartModel = pickle.load(open('HeartDModel.pkl','rb'))
-
 kidneyModel = pickle.load(open('KidneyDModel.pkl','rb'))
-
 scaler = pickle.load(open('scaler.pkl','rb'))
 scaler2 = pickle.load(open('scaler2.pkl','rb'))
 
-
+# Define routes
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -26,7 +20,6 @@ def home():
 @app.route('/model')
 def model():
     return render_template('model.html')
-
 
 @app.route('/intution')
 def intution():
@@ -44,72 +37,53 @@ def heart():
 def kidney():
     return render_template('kidney.html')
 
-@app.route('/predictDiabeties',methods=['POST'])
+# Prediction routes
+@app.route('/predictDiabeties', methods=['POST'])
 def predictDiabeties():
-
     int_features = [float(x) for x in request.form.values()]
-
-    data = np.asarray(int_features)
-
-    data = data.reshape(1,-1)
-
+    data = np.asarray(int_features).reshape(1, -1)
     std_data = scaler.transform(data)
-
-
     prediction = diabetesModel.predict(std_data)
 
     if prediction[0] == 1:
         prediction = "You are a Diabetic Person !!!"
-        color='red'
+        color = 'red'
     else:
         prediction = "You are not a Diabetic Person !!!"
-        color='green'
+        color = 'green'
 
-    return render_template('result.html',title=prediction,clr=color)
+    return render_template('result.html', title=prediction, clr=color)
 
-
-
-@app.route('/HeartPredictionResult',methods=['POST'])
+@app.route('/HeartPredictionResult', methods=['POST'])
 def HeartPredictionResult():
     int_features = [float(x) for x in request.form.values()]
-
-    data = np.asarray(int_features)
-
-    data = data.reshape(1,-1)
-
+    data = np.asarray(int_features).reshape(1, -1)
     prediction = heartModel.predict(data)
 
     if prediction[0] == 1:
-        prediction = "You Have a Heart Diesase!!!"
-        color='red'
+        prediction = "You Have a Heart Disease!!!"
+        color = 'red'
     else:
-        prediction = "You Dont Have a Heart Diesase!!!"
-        color='green'
+        prediction = "You Don't Have a Heart Disease!!!"
+        color = 'green'
 
-    return render_template('result.html',title=prediction,clr=color)
+    return render_template('result.html', title=prediction, clr=color)
 
-    
-@app.route('/KidneyPredictionResult',methods=['POST'])
+@app.route('/KidneyPredictionResult', methods=['POST'])
 def KidneyPredictionResult():
     int_features = [float(x) for x in request.form.values()]
-
-    data = np.asarray(int_features)
-
-    data = data.reshape(1,-1)
-
+    data = np.asarray(int_features).reshape(1, -1)
     std_data = scaler2.transform(data)
-
-
     prediction = kidneyModel.predict(std_data)
 
     if prediction[0] == 0:
-        prediction = "You Dont Have Chronic Kidney Diesease !!!"
-        color='green'
+        prediction = "You Don't Have Chronic Kidney Disease !!!"
+        color = 'green'
     else:
-        prediction = "You Have Chronic Kidney Diesease !!!"
-        color='red'
+        prediction = "You Have Chronic Kidney Disease !!!"
+        color = 'red'
 
-    return render_template('result.html',title=prediction,clr=color)
+    return render_template('result.html', title=prediction, clr=color)
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    app.run(debug=False, port=5001)  # For development
